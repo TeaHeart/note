def32ac1117c277faf987a9acc230903dae4f416
秉辰 韩 <1069696634@qq.com>
2023-05-13 21:21:39 +0800

    新增 T2441

---
+++ java/leetcode/src/main/java/org/example/T2441.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class T2441 {
+    public int findMaxK(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int max = -1;
+        for (int num : nums) {
+            if (set.contains(-num)) {
+                max = Math.max(max, Math.abs(num));
+            }
+            set.add(num);
+        }
+        return max;
+    }
+}
