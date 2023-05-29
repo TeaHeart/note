4f1e1f0b1b01d60e48d6b6cc0f85c25fa772c21b
秉辰 韩 <1069696634@qq.com>
2023-05-29 13:08:51 +0800

    新增 T2455

---
+++ java/leetcode/src/main/java/org/example/T2455.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class T2455 {
+    public int averageValue(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num % 6 == 0) {
+                sum += num;
+                count++;
+            }
+        }
+        return count == 0 ? 0 : sum / count;
+    }
+}
