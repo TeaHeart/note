b342fabfeb1bd3125eea8625fd7ebb95d99c0bb5
秉辰 韩 <1069696634@qq.com>
2023-06-04 12:40:40 +0800

    新增 T100

---
+++ java/leetcode/src/main/java/org/example/T100.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class T100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == q) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
