377ee9879d524b85e857589bdaba7819aeeb7f55
秉辰 韩 <1069696634@qq.com>
2023-06-09 11:32:05 +0800

    新增 T908

---
+++ java/leetcode/src/main/java/org/example/T908.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class T908 {
+    public int smallestRangeI(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return Math.max(0, max - min - k * 2);
+    }
+}
