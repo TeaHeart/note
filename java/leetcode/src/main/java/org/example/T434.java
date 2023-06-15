2c71163916f9f21bf29ec2bde3df516dea321b95
秉辰 韩 <1069696634@qq.com>
2023-06-15 08:46:14 +0800

    新增 T434

---
+++ java/leetcode/src/main/java/org/example/T434.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class T434 {
+    public int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
