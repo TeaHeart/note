51148a88df2c8c2c2b80d4df943a13295cc4485a
秉辰 韩 <1069696634@qq.com>
2023-07-01 10:50:44 +0800

    新增 M01_04

---
+++ java/leetcode/src/main/java/org/example/M01_04.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class M01_04 {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                set.remove(c);
+            } else {
+                set.add(c);
+            }
+        }
+        return set.size() <= 1;
+    }
+}
