426df0c62ed23fc9a116c9b85642ab1e951ea6b8
秉辰 韩 <1069696634@qq.com>
2023-07-08 10:23:50 +0800

    重构 T167

--- java/leetcode/src/main/java/org/example/T167.java
+++ java/leetcode/src/main/java/org/example/T167.java
@@ -1,13 +1,21 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class T167 {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            int j = Arrays.binarySearch(numbers, i + 1, numbers.length, target - numbers[i]);
-            if (j > i) {
-                return new int[]{i + 1, j + 1};
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (numbers[mid] + numbers[right] < target) {
+                left = mid + 1;
+            } else if (numbers[left] + numbers[mid] > target) {
+                right = mid - 1;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                return new int[]{left + 1, right + 1};
             }
         }
         return new int[]{-1, -1};
