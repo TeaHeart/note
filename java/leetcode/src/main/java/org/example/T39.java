7a09b0b744d06ec31bfe18052611b249366c081f
秉辰 韩 <1069696634@qq.com>
2023-07-13 11:43:01 +0800

    重构 T39

--- java/leetcode/src/main/java/org/example/T39.java
+++ java/leetcode/src/main/java/org/example/T39.java
@@ -1,26 +1,26 @@
 package org.example;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class T39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> lists = new ArrayList<>();
-        backtrack(candidates, target, 0, new ArrayDeque<>(), lists);
+        backtrack(candidates, target, 0, new ArrayList<>(), lists);
         return lists;
     }
 
-    void backtrack(int[] candidates, int target, int index, Deque<Integer> deque, List<List<Integer>> lists) {
-        if (target < 0) {
-            return;
-        }
+    void backtrack(int[] candidates, int target, int index, List<Integer> list, List<List<Integer>> lists) {
         if (target == 0) {
-            lists.add(new ArrayList<>(deque));
+            lists.add(new ArrayList<>(list));
             return;
         }
-        for (int i = index; i < candidates.length; i++) {
-            deque.offerLast(candidates[i]);
-            backtrack(candidates, target - candidates[i], i, deque, lists);
-            deque.pollLast();
+        for (int i = index; i < candidates.length && target - candidates[i] >= 0; i++) {
+            list.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i, list, lists);
+            list.remove(list.size() - 1);
         }
     }
 }
