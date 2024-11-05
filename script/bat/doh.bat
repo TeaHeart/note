11fbcda9290f9c020fbb3c59834320881f3d6ab6
TeaHeart <1069696634@qq.com>
2024-11-05 18:58:55 +0800

    新增 脚本

---
+++ script/bat/doh.bat
@@ -0,0 +1,2 @@
+netsh dns add encryption server=223.5.5.5 dohtemplate=https://dns.alidns.com/dns-query autoupgrade=yes udpfallback=no
+netsh dns add encryption server=223.6.6.6 dohtemplate=https://dns.alidns.com/dns-query autoupgrade=yes udpfallback=no
